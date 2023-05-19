aca0b37d7962fbc26c4d8d6f552595b934016ff2
William Monte de Moura <williamonte@gmail.com>
2023-05-19 16:13:18 -0300

    Exercício 03

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const fs = require("fs")
 
 const server = http.createServer((request, response) => {
     //Lógica da Função Callback.
+    switch(request.method){
+        case "GET":
+            //Lógica do GET.
+            break
+        case "POST":
+            //Lógica do POST.
+            break
+    }
 })
 
 server.listen(3000)
